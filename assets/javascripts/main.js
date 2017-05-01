d4b599bff3143bf369b6872535fe070691795ff1
Johnny Hu <jgeehen.hu@gmail.com>
2017-04-30 23:00:05 -0700

    Change search field's placeholder text based on window size

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -3,6 +3,27 @@ window.addEventListener('load', function() {
   menuIcon.addEventListener("click", function() {
     var headerMainNav = document.querySelector('.header__main-nav');
     headerMainNav.classList.toggle('show-menu');
-    console.log('hihi');
   });
+
+  var searchField = document.querySelector('.navbar__search-field');
+  if(window.innerWidth < 961) {
+    if(searchField.placeholder != "Search") {
+      searchField.placeholder = "Search";
+    }
+  }
 });
+
+
+window.addEventListener('resize', function() {
+  var searchField = document.querySelector('.navbar__search-field');
+  if(window.innerWidth < 961) {
+    if(searchField.placeholder != "Search") {
+      searchField.placeholder = "Search";
+    }
+  } else {
+    if(searchField.placeholder != "Find your next vintage camera") {
+      searchField.placeholder = "Find your next vintage camera";
+    }
+  }
+});
+
